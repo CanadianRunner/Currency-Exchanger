f9c0f49857f6cdf7a0444cd0608bc2661597fd1b
Sean Keane <seankeane93@gmail.com>
2022-06-03 16:25:32 -0700

    added image to project and resized. Also added starter text.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # template
+credit to Steve Johnson for the photograph used in this project. https://www.pexels.com/photo/assorted-silver-and-gold-colored-coins-on-gray-surface-1006060/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-// entry point for project. 
-// user interface logic.
-// Import JS/CSS files here.
-// ie: import { PeanutButter } from './peanut-butter.js'
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
