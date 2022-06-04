54d860b1a849e6b146ea0759eb6ccbda98d8c918
Sean Keane <seankeane93@gmail.com>
2022-06-03 21:04:28 -0700

    fixed css issue code wasn't properly bundling

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
-import { currencyExchangeRate } from './js/currency-exchange';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import { currencyExchangeRate } from "./js/currency-exchange";
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 
 function submitResults(response) {
-  if(response.result === 'success') {
-    $('#exchange-value').text(`Your ${response.base_code} worth ${response.conversion_result} ${response.target_code}`);
-    $('#exchange-rate').text(`The exchange rate is ${response.conversion_rate}`);
-  }
-  else {
-    if(response instanceof Error) {
-   return $('#inputted-error').text(`There was an error with this conversion: ${response.message}`);
+  if (response.result === "success") {
+    $("#exchange-value").text(
+      `Your ${response.base_code} worth ${response.conversion_result} ${response.target_code}`
+    );
+    $("#exchange-rate").text(
+      `The exchange rate is ${response.conversion_rate}`
+    );
+  } else if (response instanceof Error) {
+    $("#inputted-error").text(
+      `There was an error with this conversion: ${response.message}`
+    );
   }
 }
 
-$(document).ready(function(){
-  $('#submit').click(function(event) {
+$(document).ready(function () {
+  $("#submit").click(function (event) {
     event.preventDefault();
-    let value = $('#currency-amount').val();
-    let startingCurrency = $('#first-currency').val();
-    let convertedCurrency = $('#second-currency').val();
-    const swapButton = document.getElementById('swap-button')
-    currencyExchangeRate.currentRate(convertedCurrency, startingCurrency, value)
-    .then(function(response) {
-      submitResults(response);
-    });
-    swapButton.addEventListener('click', () => {
-      const swapCurrency = startingCurrency.value;
-       startingCurrency.value = convertedCurrency.value;
-       convertedCurrency.value = swapCurrency;
-       calculate();
-    })
-  })
+    let value = $("#currency-amount").val();
+    let startingCurrency = $("#first-currency").val();
+    let convertedCurrency = $("#second-currency").val();
+    // const swapButton = document.getElementById("swap-button");
+    currencyExchangeRate
+      .currentRate(convertedCurrency, startingCurrency, value)
+      .then(function (response) {
+        submitResults(response);
+      });
+    // swapButton.addEventListener('click', () => {
+    //   const swapCurrency = startingCurrency.value;
+    //    startingCurrency.value = convertedCurrency.value;
+    //    convertedCurrency.value = swapCurrency;
+    //    calculate();
+  });
 });
