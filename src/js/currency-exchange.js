93850dff0866a76b438d6cb1738900f29372ad8b
Sean Keane <seankeane93@gmail.com>
2022-06-03 18:36:35 -0700

    WIP CSS/Bootstrap stopped applying themselves.

--- src/js/currency-exchange.js
+++ src/js/currency-exchange.js
@@ -1,8 +1,13 @@
 export default class CurrencyExchangeRate {
   static currentRate(convertedCurrency, startingCurrency, value) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair${startingCurrency}/${convertedCurrency}/${value}`)
-    .then(function(response) {
-      
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair${startingCurrency}/${convertedCurrency}/${value}`
+    ).then(function (response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
     })
+    .catch(function)
   }
 }
