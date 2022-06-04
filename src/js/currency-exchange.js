d695f455a6b42ae2a413e47e6d1b1aa0dcde2686
Sean Keane <seankeane93@gmail.com>
2022-06-03 18:30:25 -0700

    add API fetch to currency-exchange

--- src/js/currency-exchange.js
+++ src/js/currency-exchange.js
@@ -1,3 +1,8 @@
 export default class CurrencyExchangeRate {
-  static currentRate()
+  static currentRate(convertedCurrency, startingCurrency, value) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair${startingCurrency}/${convertedCurrency}/${value}`)
+    .then(function(response) {
+      
+    })
+  }
 }
