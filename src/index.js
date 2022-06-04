4b0e97327d1e33f14d88015fdf0f4a5e4f4fb958
Sean Keane <seankeane93@gmail.com>
2022-06-03 17:58:57 -0700

    create default class and linked js files with import/export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import { CurrencyExchangeRate } from './js/currency-exchange';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+$(document).ready(function(){
+
+});
