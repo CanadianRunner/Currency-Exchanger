bd4da317d5dcd9fde84289ad41b946e8624a41a9
Sean Keane <seankeane93@gmail.com>
2022-06-03 19:12:41 -0700

    add .then and .catch to API call

--- src/js/currency-exchange.js
+++ src/js/currency-exchange.js
@@ -2,12 +2,15 @@ export default class CurrencyExchangeRate {
   static currentRate(convertedCurrency, startingCurrency, value) {
     return fetch(
       `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair${startingCurrency}/${convertedCurrency}/${value}`
-    ).then(function (response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function)
+    )
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
