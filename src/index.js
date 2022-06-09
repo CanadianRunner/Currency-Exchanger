bd73d0dc17cabf78fcafbd0f99a64e24eca1e4aa
Sean Keane <seankeane93@gmail.com>
2022-06-09 01:53:26 -0700

    updated application.  Now fully functioning to display multiple errors to the user.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
 function submitResults(response) {
-  console.log(response);
-  if ('status' in response){
-    $("#currency-fail").text(
-      `There was a problem with this currency call: ${response.status} error`
-    );
-  }
   if (response.result === "success") {
     $("#exchange-value").text(
       `Your ${response.base_code} worth ${response.conversion_result} ${response.target_code}`
@@ -18,33 +12,67 @@ function submitResults(response) {
     $("#exchange-rate").text(
       `The exchange rate is ${response.conversion_rate}`
     );
-    if (response.result === "error") {
-      $("#api-fail").text(
-        `There was a problem.  Resource not found: ${response.error_type} error`
-      );
-    }
+    $("#api-fail").text("");
   }
+}
+function displayErrors(response) {
+  if (response["error-type"] === "malformed-request") {
+    $("#api-fail").text(
+      "Sorry the API failed to return your request " + response["error-type"]
+    );
+  }
+  if (
+    response["error-type"] === "invalid-key" ||
+    response["error-type"] === "inactive-account"
+  ) {
+    $("#api-fail").text(
+      "Sorry your API Key was invalid or your account inactive " +
+        response["error-type"]
+    );
+  }
+  if (response["error-type"] === "unsupported-code") {
+    $("#api-fail").text(
+      "Sorry you didn't choose a valid currency. " + response["error-type"]
+    );
+  }
+  if (response["error-type"] === "quota-reached") {
+    $("#api-fail").text(
+      "Sorry your API key has used up its quota: " + response["error-type"]
+    );
+  }
+  $("#exchange-value").text("");
+  $("#exchange-rate").text("");
 }
 
 $(document).ready(function () {
-  let value = $("#number-one").val();
-  let startingCurrency = $("#first-currency").val();
-  let convertedCurrency = $("#second-currency").val();
-  currencyExchangeRate
-    .currentRate(convertedCurrency, startingCurrency, value)
-    .then(function (response) {
-      submitResults(response);
-    });
   $("#submit").click(function (event) {
     event.preventDefault();
     let value = $("#number-one").val();
     let startingCurrency = $("#first-currency").val();
     let convertedCurrency = $("#second-currency").val();
-    currencyExchangeRate
-      .currentRate(convertedCurrency, startingCurrency, value)
-      .then(function (response) {
-        submitResults(response);
-      });
+    let promise = currencyExchangeRate.currentRate(
+      convertedCurrency,
+      startingCurrency,
+      value
+    );
+    promise.then(
+      function (response) {
+        submitResults(JSON.parse(response));
+      },
+      function (error) {
+        if (error.status === 404) {
+          $("#api-fail").text(
+            "Please enter a valid number (positives only)! " +
+              error.status +
+              " API could not return a currency conversion. "
+          );
+          $("#exchange-value").text("");
+          $("#exchange-rate").text("");
+        } else {
+          displayErrors(JSON.parse(error.response));
+        }
+      }
+    );
   });
   $("#swap-button").click(function (event) {
     event.preventDefault();
--- src/js/currency-exchange.js
+++ src/js/currency-exchange.js
@@ -1,36 +1,17 @@
 export class currencyExchangeRate {
   static async currentRate(convertedCurrency, startingCurrency, value) {
-    try {
-      const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${startingCurrency}/${convertedCurrency}/${value}`
-      );
-      if (!response.ok) {
-        console.log(response);
-        return await response;
-      }
-      return await response.json();
-    } catch (error) {
-      return error;
-    }
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${startingCurrency}/${convertedCurrency}/${value}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request);
+        }
+      };
+      request.open("GET", url);
+      request.send();
+    });
   }
 }
-
-// export class currencyExchangeRate {
-//   static async currentRate(convertedCurrency, startingCurrency, value) {
-//     try {
-//       const response = await fetch(
-//         `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${startingCurrency}/${convertedCurrency}/${value}`
-//       );
-//       let currencyResponse;
-
-//       if (response.ok && response.status == 200) {
-//         currencyResponse = await response.json();
-//         return currencyResponse;
-//       }
-//       return await currencyResponse;
-//     } catch (error) {
-//       return error;
-//     }
-//   }
-// }
-
